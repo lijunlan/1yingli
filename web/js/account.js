139dd87d12a29a4a37ea59be47d30ac1a34e3484
zhaoy <532558098@qq.com>
2015-10-27 14:08:26 +0800

    web 前端页面修改

--- web/js/account.js
+++ web/js/account.js
@@ -2,13 +2,13 @@ $(document).ready(function(){
     var u = $.cookie('uid');
     var n = $.cookie('nickName');
     var i = $.cookie('iconUrl');
-    if(u==null||n==null||i==null){self.location='login.html';return;}
-    $("#nickName").html(n);
-    $("#bigNickName").html(n);
-    if(i!=""){
-        $("#iconUrl").attr("src",i);
-        $("#bigIcon").attr("src",i);
-    }
+    // if(u==null||n==null||i==null){self.location='login.html';return;}
+    // $("#nickName").html(n);
+    // $("#bigNickName").html(n);
+    // if(i!=""){
+    //     $("#iconUrl").attr("src",i);
+    //     $("#bigIcon").attr("src",i);
+    // }
 
     //获取电话
     refreshPhone();
