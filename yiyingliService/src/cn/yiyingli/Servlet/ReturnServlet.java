8110dbb7e0241b05d535ec7a9a73720d772bd48e
fred9582 <987097668@qq.com>
2015-10-28 11:03:22 +0800

    lp paypal将所有paypal返回信息做了检测是否非null

--- yiyingliService/src/cn/yiyingli/Servlet/ReturnServlet.java
+++ yiyingliService/src/cn/yiyingli/Servlet/ReturnServlet.java
@@ -1,6 +1,7 @@
 package cn.yiyingli.Servlet;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
@@ -24,12 +25,15 @@ import cn.yiyingli.Util.NotifyUtil;
 import cn.yiyingli.Util.WarnUtil;
 
 public class ReturnServlet extends HttpServlet {
-	
+
 	private static final long serialVersionUID = 1L;
 
 	private static String page = "http://www.1yingli.cn/yourTutor.html";
 
 	private static final String resultParameter = "?paymentResult=";
+
+	// 当payapl链接失败返回的网页
+	private static final String connectError = "<!DOCTYPE html><html lang=\"en\"><head>    <meta charset=\"UTF-8\">    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">    <title>你的导师</title>    <link rel=\"Shortcut Icon\" href=\"http://image.1yingli.cn/img/logo0.png\">    <link rel=\"Bookmark\" href=\"http://image.1yingli.cn/img/logo0.png\">    <style type=\"text/css\">    	#succ{    		width: 400px;height: 200px;margin: auto;position: fixed;top: 50%;left: 50%;margin-top:-150px;margin-left:-200px;border-radius: 10px;z-index: 101;background: #fff; border:1px solid #D2D2D2    	}    	.succ_title{    	  width: 400px;height: 35px;background-color: #d2d2d2;border-top-left-radius: 10px;border-top-right-radius: 10px;text-align: center;    	}    	.succ_title div{    	  font-size: 16px;padding-top: 5px;font-weight: bold;color:#FFF;    	}    	.succ_content{    		position: absolute;top: 40%;left: 33%;font-size: 20px;color: #b6b6b6;    	}    	#succ a{    		text-decoration: none;width: 128px;position: absolute;top: 70%;left: 35%;font-size: 20px;color: #FFF;background-color: #56bbe8;text-align: center;border-radius: 14px;    	}    </style></head><body>	<div id=\"succ\">    	<div class=\"succ_title\"><div>来自一英里的信息</div></div>    	<div class=\"succ_content\">支付连接超时，请重新支付。</div>    	<a href=\"http://www.1yingli.cn/yourTutor.html\">确定</a>		</div></body>";
 
 	private ApplicationContext applicationContext;
 
@@ -76,8 +80,18 @@ public class ReturnServlet extends HttpServlet {
 			 * Calls the GetExpressCheckoutDetails API call
 			 */
 			Map<String, String> results = pp.getShippingDetails(token);
+			//检测paypal是否链接中断，没有返回内容
+			if (results == null) {
+				returnMsg(response, connectError);
+				return;
+			}
 			// 根据之前传的callback参数修改callback
-			String tmp = results.get("PAYMENTREQUEST_0_CUSTOM");
+			String tmp = "";
+			if (results.get("PAYMENTREQUEST_0_CUSTOM") != null) {
+				tmp = results.get("PAYMENTREQUEST_0_CUSTOM");
+			} else if (results.get("CUSTOM") != null) {
+				tmp = results.get("CUSTOM");
+			}
 			String callback[] = tmp.split("\\|");
 			if (callback.length != 1) {
 				page = callback[1];
@@ -146,6 +160,11 @@ public class ReturnServlet extends HttpServlet {
 				// sanitized before being used.
 				@SuppressWarnings("rawtypes")
 				HashMap results = pp.confirmPayment(checkoutDetails, request.getServerName());
+				//检测paypal是否链接中断，没有返回内容
+				if (results == null) {
+					returnMsg(response, connectError);
+					return;
+				}
 				request.setAttribute("payment_method", "");
 				String strAck = results.get("ACK").toString().toUpperCase();
 				if (strAck != null
@@ -275,6 +294,24 @@ public class ReturnServlet extends HttpServlet {
 			response.sendRedirect(page + para);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * return message through httpResponse
+	 * 
+	 * @param msg
+	 */
+	private void returnMsg(HttpServletResponse resp, String msg) {
+		resp.setCharacterEncoding("UTF-8");
+		resp.setHeader("Content-type", "text/html;charset=UTF-8");
+		resp.setHeader("Access-Control-Allow-Origin", "*");
+		try {
+			OutputStream stream = resp.getOutputStream();
+			LogUtil.info("send>>>>>" + msg, this.getClass());
+			stream.write(msg.getBytes("UTF-8"));
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
