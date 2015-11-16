9651a65dfb6a0cd0fb0f0e0d5f5ae6645788b328
ryan <503775540@qq.com>
2015-11-16 15:45:06 +0800

    search

--- web/js/common.js
+++ web/js/common.js
@@ -1,4 +1,17 @@
-﻿<!--for Baidu-->
+﻿var word = "";//主题搜索字
+var page = "";//页码
+var sort = "";//排序
+var id = "";//所选这的主题
+var findName1 = "";//搜索关键字
+var findName = "";//搜索关键字
+var tips1;
+var tips2;
+var tips3;
+var tips4;
+var tips5;
+
+
+<!--for Baidu-->
 var _hmt = _hmt || [];
 (function () {
     var hm = document.createElement("script");
@@ -77,10 +90,6 @@ $(document).ready(function () {
     $('.sidenav1 a').click(function () {
         $(".right").click();
     });
-    //$(".header").css("margin","10px");
-    //1...10分别代表不同的id请求参数。
-    //$(".contact").html("<div class='contact-content'><div class='contact-left'> <ul class='contact-left-top'><li><a href='joinus.html?id=1'>关于我们</a></li><li><span>/</span></li> <li><a href='joinus.html?id=1'>加入我们</a></li><li><span>/</span></li><li><a href='joinus.html?id=1'>联系我们</a></li></ul><ul class='contact-left-left'><li><a href='joinus.html?id=4'>团队故事</a></li><li><a href='joinus.html?id=5'>团队成员</a></li><li><a href='joinus.html?id=1'>媒体报道</a></li></ul><ul class='contact-left-center'><li><a href='joinus.html?id=1'>招聘岗位</a></li><li><a href='joinus.html?id=1'>公司福利</a></li></ul><ul class='contact-left-right'><li><a href='joinus.html?id=1'>网站反馈</a></li><li><a href='joinus.html?id=1'>商务合作</a></li></ul></div><div class='contact-right'><p class='contact-right-1'>客服电话：</p><p class='contact-right-2'>0571-86415101</p><p class='contact-right-3'>工作日：9:00-17:00</p><p class='contact-right-4'>客服邮箱：contact@1yingli.cn</p></div></div>");
-    //$(".footer").html("<ul class='footer-share'> <li><a href='home.html'><img src='http://image.1yingli.cn/img/footer_icon.jpg' class='footer-icon'/></a></li><li><img src='http://image.1yingli.cn/img/footer_1.jpg'/></li><li><a class='icon_img_tip'  href=''><img id='footer-wechat' src='http://image.1yingli.cn/img/footer_wechat.png'/></a><!--<div class='icon_tip' id='wechat-tip'></div>--></li> <li><a class='icon_img_tip'  href=''><img id='footer-in' src='http://image.1yingli.cn/img/footer_in.png'/></a><!--<div class='icon_tip' id='in-tip' ></div>--></li><li><a class='icon_img_tip' href=''><img id='ooter-ren' src='http://image.1yingli.cn/img/footer_ren.png'/></a><!--<div class='icon_tip' id='ren-tip' ></div>--></li><li><a class='icon_img_tip'  href=''><img id='footer-weibo' src='http://image.1yingli.cn/img/footer_weibo.png'/></a><!--<div class='icon_tip' id='weibo-tip' ></div>--></li></ul> <p class='copyright'>Copyright@2014-2015 杭州千询科技有限公司 版权所有<a href='http://www.miitbeian.gov.cn/'  target='_Blank'>浙ICP备15023254号</a></p>");
     $(".footer").html("<div id='ustel'><div class='footerc' style='width:1024px;margin:0 auto; height:100%'><div id='wximg' style='width:200px;float:left;margin-top:50px;margin-left:100px'><img src='http://image.1yingli.cn/img/WeChat_1440487276.jpeg' width=150px; height=150px style='border-radius: 8px;'></img><p style='width:150px;color:#fff;font-size:16px; text-align:center;line-height:50px;'>微信公众账号</p></div><div id='company' style='width:350px;;float:left;margin-top:40px;margin-left:300px;font-size:18px;color:#fff;align:right;text-align: right;line-height: 40px;'> <p id='aboutUs'><a href='joinus.html?id=1' style='color:#fff;text-decoration:none;'>关于我们</a> / <a href='joinus.html?id=2' style='color:#fff;text-decoration:none;'>加入我们</a> / <a href='joinus.html?id=3' style='color:#fff;text-decoration:none;'>联系我们</a></p><p class='phonenum'>0571-86415101</p><p id='workday1'>工作日：9:30-17:30 (GMT+8)</p><p id='workday2'>工作日：9:30-17:30</p><p class='emailbt'>客服邮箱：contact@1yingli.cn</p><p id='companynu1' style='width:100%;height:60px;float:left;display:none;'><a style='color:#FFF;' href='http://www.miitbeian.gov.cn/'  target='_Blank'>©2015 杭州千询科技 浙ICP备15023254号</a></p></div></div></div>"
         + "<div id='botx' style='width:100%;height:3px;float:left;opacity: 0.3;border-top: 1px solid #ccc;'></div>"
         + "<div id='companynu2' style='width:100%;height:60px;float:left'><div class='number' style='width:1024px;margin:0 auto;'><p style='height:60px;line-height:60px;color:white;text-align: center;' ><a style='color:#FFF;' href='http://www.miitbeian.gov.cn/'  target='_Blank'>©2015 杭州千询科技 浙ICP备15023254号</a></p></div></div>");
@@ -115,8 +124,9 @@ $(document).ready(function () {
 
 });
 
-var hit = 10; // 显示的下拉提示列表的长度
+var hit = 10;
 setTimeout(function () {
+    //手机端头部搜索
     if ($("#mobile_search").length > 0) {
         $("#mobile_search").autocomplete({
             delay: 0,
@@ -149,45 +159,63 @@ setTimeout(function () {
             self.location = 'search.html?findName=' + encodeURIComponent(encodeURIComponent(ui.item.value));
         }).focus();
     }
+
+    //pc端搜索头部搜索
+    $("#search-input").autocomplete({
+        delay: 0,
+        source: function(request, response) {
+            var toSend = new Object();
+            toSend.style = "function";
+            toSend.method = "getSearchHint";
+            toSend.word = $( "#search-input" ).val();
+            $.ajax({
+                url: config.base_url,
+                type : "POST",
+                dataType: 'json',
+                data: $.toJSON(toSend),
+                success: function(data) {
+                    if(data.status === 'OK' && data.result) {
+                        if(data.result.length >= hit) {
+                            response(data.result.slice(0, hit));
+                        } else {
+                            response(data.result);
+                        }
+                    } else if( data.status === 'FAIL' && data.errors ){
+                    }
+                }
+            });
+        }
+    }).bind("input.autocomplete", function () {
+        $( "#search-input" ).autocomplete("search", $( "#search-input" ).val());
+    }).bind('autocompleteselect', function(e,ui) {
+        self.location = 'search.html?findName=' + encodeURIComponent(encodeURIComponent(ui.item.value));
+    }).focus();
 });
 
+//手机端头部搜索框
 $('#mobile_search_button').click(function(){
     self.location = 'search.html?findName=' + encodeURIComponent(encodeURIComponent($("#mobile_search").val()));
 });
 
+//获取搜索界面url参数
+function getParameter(){
+    word = encodeURIComponent($_GET('word'));//主题搜索字
+    page = $_GET('page');//页码
+    sort = $_GET('sort');//排序
+    id = $_GET('id');//所选这的主题
+    findName = encodeURIComponent($_GET('findName'));//搜索关键字
+    findName1 = encodeURIComponent($_GET('findName1'));//搜索关键字
+    tips1 = encodeURIComponent($_GET('tip1'));
+    tips2 = encodeURIComponent($_GET('tip2'));
+    tips3 = encodeURIComponent($_GET('tip3'));
+    tips4 = encodeURIComponent($_GET('tip4'));
+    tips5 = encodeURIComponent($_GET('tip5'));
+}
 
 //首页主题搜索
 function search() {
     var findName = encodeURIComponent(encodeURIComponent($("#searchBox").val()));
     self.location = 'search.html?findName=' + findName;
-}
-
-//头部搜索
-function searchByInput() {
-    var findName = encodeURIComponent(encodeURIComponent($("#search-input").val()));
-    self.location = 'search.html?findName=' + findName;
-}
-
-//头部搜索
-function searchByInput1() {
-    var findName1 = encodeURIComponent(encodeURIComponent($("#search-input1").val()));
-    self.location = 'search.html?findName1=' + findName1;
-}
-
-function KeyDown() {
-    if (event.keyCode == 13) {
-        event.returnValue = false;
-        event.cancel = true;
-        searchByInput();
-    }
-}
-
-function KeyDown1() {
-    if (event.keyCode == 13) {
-        event.returnValue = false;
-        event.cancel = true;
-        searchByInput1();
-    }
 }
 
 //主题搜索
@@ -196,6 +224,37 @@ function findByTheme(themeName, id) {
     self.location = 'search.html?word=' + w + '&id=' + id;
 }
 
+//页面头部搜索框
+function searchByInput() {
+    getParameter();
+    findName = encodeURIComponent(encodeURIComponent($("#search-input").val()));
+    self.location = 'search.html?tip1='+tips1+'&tip2='+tips2+'&tip3='+tips3+'&tip4='+tips4+'&tip5='+tips5+'&word='+word+'&page='+page+'&findName='+findName+'&findName1='+findName1+'&sort='+sort+'&id='+id;
+    return;
+}
+
+//页面中间搜索框
+function searchByInput1() {
+    getParameter();
+    findName1 = encodeURIComponent(encodeURIComponent($("#search-input1").val()));
+    self.location = 'search.html?tip1='+tips1+'&tip2='+tips2+'&tip3='+tips3+'&tip4='+tips4+'&tip5='+tips5+'&word='+word+'&page='+page+'&findName='+findName+'&findName1='+findName1+'&sort='+sort+'&id='+id;
+    return;
+}
+function KeyDown() {
+    if (event.keyCode == 13) {
+        event.returnValue = false;
+        event.cancel = true;
+        searchByInput();
+    }
+}
+function KeyDown1() {
+    if (event.keyCode == 13) {
+        event.returnValue = false;
+        event.cancel = true;
+        searchByInput1();
+    }
+}
+
+//接口配置路劲
 var config = {
     base_url: "http://service.1yingli.cn/yiyingliService/manage"
 };
@@ -204,6 +263,7 @@ $('.subject').append(
     '<div id="bg" class="bg"/>'
 );
 
+//退出登录
 function logout(){
     $.ajax({
         cache : true,
--- web/js/person_header.js
+++ web/js/person_header.js
@@ -27,70 +27,7 @@
     <!--header break -->
     //tutorSheet.html
     $(".top-nav").html("<div class='nav-center'><ul><li><a href='controlPanel.html'>消息中心</a></li><li><a href='yourTutor.html'>我的导师</a></li><li><a href='tutor.html'>导师中心</a></li><li><a href='general.html'>个人资料</a></li><li> <a href='likeList.html'>心愿名单</a> </li></ul></div>");
-
-    //getEmailCount();
-
-    //下拉菜单
-    var hit = 10; // 显示的下拉提示列表的长度
-    setTimeout(function() {
-        $("#search-input").autocomplete({
-            delay: 0,
-            source: function(request, response) {
-                var toSend = new Object();
-                toSend.style = "function";
-                toSend.method = "getSearchHint";
-                toSend.word = $( "#search-input" ).val();
-                $.ajax({
-                    url: config.base_url,
-                    type : "POST",
-                    dataType: 'json',
-                    data: $.toJSON(toSend),
-                    success: function(data) {
-                        if(data.status === 'OK' && data.result) {
-                            if(data.result.length >= hit) {
-                                response(data.result.slice(0, hit));
-                            } else {
-                                response(data.result);
-                            }
-                        } else if( data.status === 'FAIL' && data.errors ){
-                            //alert(data.errors[0].message);
-                        }
-                    }
-                });
-            }
-        }).bind("input.autocomplete", function () {
-            $( "#search-input" ).autocomplete("search", $( "#search-input" ).val());
-        }).bind('autocompleteselect', function(e,ui) {
-            self.location = 'search.html?findName=' + encodeURIComponent(encodeURIComponent(ui.item.value));
-        }).focus();
-    }, 0);
 });
-
-function logout(){
-    $.ajax({
-        cache : true,
-        type : "POST",
-        url : config.base_url,
-        data : "{'style':'user','method':'logout','uid':'" + $.cookie('uid') + "'}",
-        async : false,
-        error : function(request) {
-                    alert("Connection error");
-                },
-        success : function(data, textStatu) {
-            var json = eval("(" + data + ")");
-            if (json.state == "success") {
-                $.cookie("uid",'',{expires: -1,path:'/',domain:".1yingli.cn",secure:false,raw:false});
-                $.cookie("nickName",'',{expires: -1,path:'/',domain:".1yingli.cn",secure:false,raw:false});
-                $.cookie("iconUrl",'',{expires: -1,path:'/',domain:".1yingli.cn",secure:false,raw:false});
-                $.cookie("tid",'',{expires: -1,path:'/',domain:".1yingli.cn",secure:false,raw:false});
-                self.location='home.html';
-                return;
-            } else {
-                alert(json.msg);
-            }
-        }
-    });
-}
 
 function getEmailCount() {
     $.ajax({
