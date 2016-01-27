01dfa0c4a5e38d9d4a7bf0f69e8ecda9efd7076f
lijunlan <lijunlan@1yingli.cn>
2016-01-27 17:49:50 +0800

    SERVICE:FIXED

--- yiyingliService/src/cn/yiyingli/Handle/Service/CancelOrderService.java
+++ yiyingliService/src/cn/yiyingli/Handle/Service/CancelOrderService.java
@@ -1,13 +1,15 @@
 package cn.yiyingli.Handle.Service;
 
 import cn.yiyingli.Handle.UMsgService;
-import cn.yiyingli.Persistant.Order;
+import cn.yiyingli.Persistant.OrderList;
 import cn.yiyingli.Persistant.User;
 import cn.yiyingli.Service.NotificationService;
+import cn.yiyingli.Service.OrderListService;
 import cn.yiyingli.Service.OrderService;
 import cn.yiyingli.Util.MsgUtil;
 import cn.yiyingli.Util.NotifyUtil;
 import cn.yiyingli.Util.AlipayCancelUtil;
+import cn.yiyingli.Util.LogUtil;
 
 /**
  * 取消订单，仅存在于还没生成支付宝订单的情况下
@@ -17,16 +19,16 @@ import cn.yiyingli.Util.AlipayCancelUtil;
  */
 public class CancelOrderService extends UMsgService {
 
-	private OrderService orderService;
+	private OrderListService orderListService;
 
 	private NotificationService notificationService;
 
-	public OrderService getOrderService() {
-		return orderService;
+	public OrderListService getOrderListService() {
+		return orderListService;
 	}
 
-	public void setOrderService(OrderService orderService) {
-		this.orderService = orderService;
+	public void setOrderListService(OrderListService orderListService) {
+		this.orderListService = orderListService;
 	}
 
 	public NotificationService getNotificationService() {
@@ -39,42 +41,45 @@ public class CancelOrderService extends UMsgService {
 
 	@Override
 	protected boolean checkData() {
-		return super.checkData() && getData().containsKey("orderId");
+		return super.checkData() && getData().containsKey("olid");
 	}
 
 	@Override
 	public void doit() {
 		User user = getUser();
-		String oid = (String) getData().get("orderId");
-		Order order = getOrderService().queryByShowId(oid, false);
-		if (order == null) {
-			setResMsg(MsgUtil.getErrorMsgByCode("42001"));
+		String olid = (String) getData().get("olid");
+		OrderList orderList = getOrderListService().queryByOrderListNo(olid);
+		if (orderList == null) {
+			setResMsg(MsgUtil.getErrorMsgByCode("42003"));
 			return;
 		}
-		if (order.getCreateUser().getId().longValue() != user.getId().longValue()) {
+		if (orderList.getUser().getId().longValue() != user.getId().longValue()) {
+			LogUtil.info("receive>>>>cancelOrderListId:" + orderList.getUser().getId() + ",userId:" + user.getId() + ","
+					+ (orderList.getUser().getId().longValue() == user.getId().longValue()), this.getClass());
 			setResMsg(MsgUtil.getErrorMsgByCode("44001"));
 			return;
 		}
-		String state = order.getState().split(",")[0];
+		String state = orderList.getState().split(",")[0];
 		if (!OrderService.ORDER_STATE_NOT_PAID.equals(state)) {
 			setResMsg(MsgUtil.getErrorMsgByCode("44002"));
 			return;
 		}
-		order.setState(OrderService.ORDER_STATE_CANCEL_PAID + "," + order.getState());
+
+		orderList.setState(OrderListService.ORDER_STATE_CANCEL_PAID + "," + orderList.getState());
 
 		try {
-			if (AlipayCancelUtil.cancelOrder("", oid)) {
-				order.setState(OrderService.ORDER_STATE_END_FAILED + "," + order.getState());
-				getOrderService().update(order, false);
-				NotifyUtil.notifyUserOrder(order, "尊敬的学员，您的订单已经取消。订单号" + order.getOrderNo(), user,
+			if (AlipayCancelUtil.cancelOrder("", olid)) {
+				orderList.setState(OrderService.ORDER_STATE_END_FAILED + "," + orderList.getState());
+				getOrderListService().update(orderList);
+				NotifyUtil.notifyUserOrder(orderList, "尊敬的学员，您的订单已经取消。订单号" + orderList.getOrderListNo(), user,
 						getNotificationService());
-				setResMsg(MsgUtil.getSuccessMsg("cancel order successfully"));
+				setResMsg(MsgUtil.getSuccessMsg("cancel orderList successfully"));
 				return;
 			} else {
-				getOrderService().update(order, false);
-				NotifyUtil.notifyUserOrder(order, "尊敬的学员，您的订单已经取消。订单号" + order.getOrderNo(), user,
+				getOrderListService().update(orderList);
+				NotifyUtil.notifyUserOrder(orderList, "尊敬的学员，您的订单已经取消。订单号" + orderList.getOrderListNo(), user,
 						getNotificationService());
-				setResMsg(MsgUtil.getSuccessMsg("cancel order successfully"));
+				setResMsg(MsgUtil.getSuccessMsg("cancel orderList successfully"));
 				return;
 			}
 		} catch (Exception e) {
