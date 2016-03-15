166981f45ba5bb5789f79389607033c14bb9979d
lijunlan <lijunlan@1yingli.cn>
2016-03-15 15:54:11 +0800

    SERVICE:FIXED HOME PAGE TEACHER DATA

--- yiyingliService/src/cn/yiyingli/Handle/Service/FGetHomeTeacherListService.java
+++ yiyingliService/src/cn/yiyingli/Handle/Service/FGetHomeTeacherListService.java
@@ -2,26 +2,27 @@ package cn.yiyingli.Handle.Service;
 
 import java.util.List;
 
-import cn.yiyingli.ExchangeData.ExTeacher;
+import cn.yiyingli.ExchangeData.ExContentAndPage;
 import cn.yiyingli.ExchangeData.SuperMap;
 import cn.yiyingli.ExchangeData.Util.ExArrayList;
 import cn.yiyingli.ExchangeData.Util.ExList;
 import cn.yiyingli.Handle.MsgService;
-import cn.yiyingli.Persistant.Teacher;
+import cn.yiyingli.Persistant.ContentAndPage;
+import cn.yiyingli.Service.ContentAndPageService;
 import cn.yiyingli.Service.PagesService;
 import cn.yiyingli.Service.TeacherService;
 import cn.yiyingli.Util.MsgUtil;
 
 public class FGetHomeTeacherListService extends MsgService {
 
-	private TeacherService teacherService;
+	private ContentAndPageService contentAndPageService;
 
-	public TeacherService getTeacherService() {
-		return teacherService;
+	public ContentAndPageService getContentAndPageService() {
+		return contentAndPageService;
 	}
 
-	public void setTeacherService(TeacherService teacherService) {
-		this.teacherService = teacherService;
+	public void setContentAndPageService(ContentAndPageService contentAndPageService) {
+		this.contentAndPageService = contentAndPageService;
 	}
 
 	@Override
@@ -31,12 +32,12 @@ public class FGetHomeTeacherListService extends MsgService {
 
 	@Override
 	public void doit() {
-		List<Teacher> teachers = getTeacherService().queryListByActivity(PagesService.KEY_HOME_TEACHER, 1,
-				TeacherService.HOME_PAGE_SIZE);
+		List<ContentAndPage> contentAndPages = getContentAndPageService()
+				.queryListWithTeacherByKey(PagesService.KEY_HOME_TEACHER, 1, TeacherService.HOME_PAGE_SIZE);
 		ExList exTeachers = new ExArrayList();
-		for (Teacher teacher : teachers) {
+		for (ContentAndPage contentAndPage : contentAndPages) {
 			SuperMap map = new SuperMap();
-			ExTeacher.assembleSimpleForUser(teacher, map);
+			ExContentAndPage.assembleTeacherForUser(contentAndPage, map);
 			exTeachers.add(map.finish());
 		}
 		setResMsg(MsgUtil.getSuccessMap().put("data", exTeachers).finishByJson());
