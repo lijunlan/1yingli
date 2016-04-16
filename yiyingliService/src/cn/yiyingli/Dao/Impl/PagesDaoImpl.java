6162590689f866d07ee77001f669aad1c04ab1d2
wpexia <wpexia@gmail.com>
2016-04-16 10:17:44 +0800

    添加管理员查看活动文章

--- yiyingliService/src/cn/yiyingli/Dao/Impl/PagesDaoImpl.java
+++ yiyingliService/src/cn/yiyingli/Dao/Impl/PagesDaoImpl.java
@@ -5,7 +5,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import cn.yiyingli.Dao.ContentAndPageDao;
+import cn.yiyingli.Dao.PassageDao;
+import cn.yiyingli.Persistant.ContentAndPage;
 import cn.yiyingli.Persistant.Passage;
+import cn.yiyingli.Persistant.Teacher;
 import org.hibernate.HibernateException;
 import org.hibernate.Query;
 import org.hibernate.Session;
@@ -121,10 +124,14 @@ public class PagesDaoImpl extends HibernateDaoSupport implements PagesDao {
 		list = getHibernateTemplate().executeFind(new HibernateCallback<List<Passage>>() {
 			@Override
 			public List<Passage> doInHibernate(Session session) throws HibernateException, SQLException {
-				String hql = "from Passage p left join p.ownTeacher as t" +
-						" where t.id in" +
-						"(select cap.teacher.id from ContentAndPage cap inner join cap.teacher as tt" +
-						" left join cap.pages as pa where pa.id = " + id + ")";
+				String hql = "from Passage p left join fetch p.ownTeacher inner join fetch p.ownTeacher.contentAndPages as cap" +
+						" left join fetch cap.pages pa where pa.id = " + id + " and cap.style = " +
+						ContentAndPageDao.STYLE_TEACHER + " and p.state=" + PassageDao.PASSAGE_STATE_OK +
+						" and p.onshow=" + true + " and p.remove=" + false;
+//				String hql = "from ContentAndPage cap left join fetch cap.teacher" +
+//						" left join cap.pages as pa" +
+//						" left join fetch cap.teacher.passages" +
+//						" where pa.id = " + id + " and cap.style = " + ContentAndPageDao.STYLE_TEACHER;
 				Query query = session.createQuery(hql);
 				query.setFirstResult((page - 1) * pageSize);
 				query.setMaxResults(pageSize);
@@ -139,12 +146,16 @@ public class PagesDaoImpl extends HibernateDaoSupport implements PagesDao {
 	@Override
 	public List<Passage> queryPassageListById(final long id, final int page, final int pageSize) {
 		List<Passage> list = new ArrayList<>();
-		list = getHibernateTemplate().executeFind(new HibernateCacllback<List<Passage>>() {
+		list = getHibernateTemplate().executeFind(new HibernateCallback<List<Passage>>() {
 			@Override
 			public List<Passage> doInHibernate(Session session) throws HibernateException, SQLException {
-				String hql = "select cap.passage from ContentAndPages cap left join fetch cap.passage as p" +
-						" left join cap.pages as pa where pa.id = " + id + " and cap.style = " +
-						ContentAndPageDao.STYLE_TEACHER;
+				String hql = "from Passage p left join fetch p.ownTeacher left join fetch p.contentAndPages as" +
+						" cap left join fetch cap.pages as pa where pa.id = " + id + " and cap.style = "
+						+ ContentAndPageDao.STYLE_PASSAGE + " and p.state=" + PassageDao.PASSAGE_STATE_OK +
+						" and p.onshow=" + true + " and p.remove=" + false;
+//				String hql = "select cap.passage from ContentAndPage cap left join fetch cap.passage as p" +
+//						" left join cap.pages as pa where pa.id = " + id + " and cap.style = " +
+//						ContentAndPageDao.STYLE_TEACHER;
 				Query query = session.createQuery(hql);
 				query.setFirstResult((page - 1) * pageSize);
 				query.setMaxResults(pageSize);
