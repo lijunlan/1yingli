faf4700b96eab5b4c15f97daa190d25a4ba67c84
wpexia <wpexia@gmail.com>
2016-04-21 11:55:02 +0800

    add wechat no to message

--- yiyingliService/src/cn/yiyingli/Util/NotifyUtil.java
+++ yiyingliService/src/cn/yiyingli/Util/NotifyUtil.java
@@ -24,46 +24,46 @@ public class NotifyUtil {
 		Teacher teacher = order.getTeacher();
 		String nowState = states[0];
 		if (operation.equals(MOrderSalaryDoneService.class.getName())) {
-			return "尊敬的用户，订单(订单号:" + No + "),酬劳已成功转出，请注意查收.如有疑问请咨询客服.";
+			return "尊敬的用户，订单(订单号:" + No + "),酬劳已成功转出，请注意查收.如有疑问请联系小助手,小助手微信号:yiyinglikeke";
 		}
 		if (states.length > 1) {
 			String lastState = states[1];
 			if (nowState.equals(OrderService.ORDER_STATE_USER_REGRET)
 					&& lastState.equals(OrderService.ORDER_STATE_WAIT_ENSURETIME)) {
-				return "尊敬的用户，订单(订单号:" + No + "),客户申请退款，等待您的确认，请在5天内到平台进行操作，超时系统会自动同意退款。";
+				return "尊敬的用户，订单(订单号:" + No + "),客户申请退款，等待您的确认，请在5天内到平台进行操作，超时系统会自动同意退款。如有疑问请联系小助手,小助手微信号:yiyinglikeke";
 			} else if (nowState.equals(OrderService.ORDER_STATE_USER_REGRET)
 					&& lastState.equals(OrderService.ORDER_STATE_WAIT_SERVICE)) {
 				if (operation.equals(TRefuseOrderService.class.getName())) {
 					return "尊敬的用户，订单(订单号:" + No + ")已被[" + teacher.getName() + "]拒绝,拒绝理由:" + order.getRefuseReason()
-							+ ",您可预约其他优秀的导师哦,预付款将在24小时内退还到您的账户。";
+							+ ",您可预约其他优秀的导师哦,预付款将在24小时内退还到您的账户。如有疑问请联系小助手,小助手微信号:yiyinglikeke";
 				} else {
-					return "尊敬的用户，订单(订单号:" + No + "),客户申请退款，等待您的确认。请在5天内到平台进行操作，超时系统会自动同意退款。";
+					return "尊敬的用户，订单(订单号:" + No + "),客户申请退款，等待您的确认。请在5天内到平台进行操作，超时系统会自动同意退款。如有疑问请联系小助手,小助手微信号:yiyinglikeke";
 				}
 			} else if (nowState.equals(OrderService.ORDER_STATE_WAIT_RETURN)
 					&& lastState.equals(OrderService.ORDER_STATE_FINISH_PAID)) {
-				return "尊敬的用户，订单(订单号:" + No + "),已经被用户取消。";
+				return "尊敬的用户，订单(订单号:" + No + "),已经被用户取消。如有疑问请联系小助手,小助手微信号:yiyinglikeke";
 			} else if (nowState.equals(OrderService.ORDER_STATE_USER_DISLIKE)
 					&& lastState.equals(OrderService.ORDER_STATE_SERVICE_FINISH)) {
-				return "尊敬的用户，订单(订单号:" + No + "),对本次服务不满,申请退款,等待您的回复,请您在五天内到平台进行操作,超时系统将自动同意退款.";
+				return "尊敬的用户，订单(订单号:" + No + "),对本次服务不满,申请退款,等待您的回复,请您在五天内到平台进行操作,超时系统将自动同意退款.如有疑问请联系小助手,小助手微信号:yiyinglikeke";
 			} else if (nowState.equals(OrderService.ORDER_STATE_SERVICE_FINISH)
 					&& lastState.equals(OrderService.ORDER_STATE_WAIT_SERVICE)) {
 				// return "尊敬的用户，订单(订单号:" + No + ")已自动确认服务完毕,请等待用户确认本次服务.";
 				return "";
 			} else if (nowState.equals(OrderService.ORDER_STATE_WAIT_RETURN)
 					&& lastState.equals(OrderService.ORDER_STATE_USER_DISLIKE)) {
-				return "尊敬的用户，订单(订单号:" + No + "),由于您长时间未作出响应,系统已自动确认同意退款,如有疑问,请与客服联系.";
+				return "尊敬的用户，订单(订单号:" + No + "),由于您长时间未作出响应,系统已自动确认同意退款,如有疑问请联系小助手,小助手微信号:yiyinglikeke";
 			} else if (nowState.equals(OrderService.ORDER_STATE_WAIT_RETURN)
 					&& lastState.equals(OrderService.ORDER_STATE_USER_REGRET)) {
-				return "尊敬的用户，订单(订单号:" + No + "),由于您长时间未作出响应,系统已自动确认同意退款,如有疑问,请与客服联系.";
+				return "尊敬的用户，订单(订单号:" + No + "),由于您长时间未作出响应,系统已自动确认同意退款,如有疑问请联系小助手,小助手微信号:yiyinglikeke";
 			} else if (nowState.equals(OrderService.ORDER_STATE_WAIT_COMMENT)
 					&& lastState.equals(OrderService.ORDER_STATE_MANAGER_IN)) {
-				return "尊敬的用户，订单(订单号:" + No + "),客服已经协调结束，协调结果：拒绝此次退款。您的酬劳会在24小时内到账.";
+				return "尊敬的用户，订单(订单号:" + No + "),客服已经协调结束，协调结果：拒绝此次退款。您的酬劳会在24小时内到账.如有疑问请联系小助手,小助手微信号:yiyinglikeke";
 			} else if (nowState.equals(OrderService.ORDER_STATE_END_FAILED)
 					&& lastState.equals(OrderService.ORDER_STATE_MANAGER_IN)) {
-				return "尊敬的用户，订单(订单号:" + No + "),客服已经协调结束，协调结果：拒绝此次退款。您的酬劳会在24小时内到账.";
+				return "尊敬的用户，订单(订单号:" + No + "),客服已经协调结束，协调结果：拒绝此次退款。您的酬劳会在24小时内到账.如有疑问请联系小助手,小助手微信号:yiyinglikeke";
 			} else if (nowState.equals(OrderService.ORDER_STATE_WAIT_RETURN)
 					&& lastState.equals(OrderService.ORDER_STATE_MANAGER_IN)) {
-				return "尊敬的用户，订单(订单号:" + No + "),客服已经协调结束，协调结果：同意此次退款。";
+				return "尊敬的用户，订单(订单号:" + No + "),客服已经协调结束，协调结果：同意此次退款。如有疑问请联系小助手,小助手微信号:yiyinglikeke";
 			} else if (nowState.equals(OrderService.ORDER_STATE_WAIT_ENSURETIME)
 					&& lastState.equals(OrderService.ORDER_STATE_FINISH_PAID)) {
 //				return "尊敬的用户，订单(订单号:" + No + ")已被您接受,请及时与客户联系并确认服务时间,之后请到一英里平台登记服务时间。(客户姓名:" + order.getCustomerName()
@@ -85,7 +85,7 @@ public class NotifyUtil {
 				return "";
 			} else if (nowState.equals(OrderService.ORDER_STATE_FINISH_PAID)
 					&& lastState.equals(OrderService.ORDER_STATE_NOT_PAID)) {
-				return "尊敬的用户，订单(订单号:" + No + ")，客户(" + order.getCustomerName() + ")已经付款，等待您的接受。";
+				return "尊敬的用户，订单(订单号:" + No + ")，客户(" + order.getCustomerName() + ")已经付款，等待您的接受。如有疑问请联系小助手,小助手微信号:yiyinglikeke";
 			}else {
 				return "";
 			}
@@ -101,7 +101,7 @@ public class NotifyUtil {
 			String lastState = states[1];
 			if (nowState.equals(OrderListService.ORDER_STATE_CANCEL_PAID)
 					&& lastState.equals(OrderListService.ORDER_STATE_NOT_PAID)) {
-				return "尊敬的用户，订单(流水号:" + No + ")由于超时未支付,已被系统自动关闭,如有需要,请重新预约。";
+				return "尊敬的用户，订单(流水号:" + No + ")由于超时未支付,已被系统自动关闭,如有需要,请重新预约。如有疑问请联系小助手,小助手微信号:yiyinglikeke";
 			} else {
 				return "";
 			}
@@ -119,64 +119,64 @@ public class NotifyUtil {
 			String lastState = states[1];
 			if (nowState.equals(OrderService.ORDER_STATE_USER_REGRET)
 					&& lastState.equals(OrderService.ORDER_STATE_WAIT_ENSURETIME)) {
-				return "尊敬的用户，订单(订单号:" + No + ")已经申请取消,请等待[" + teacher.getName() + "]同意";
+				return "尊敬的用户，订单(订单号:" + No + ")已经申请取消,请等待[" + teacher.getName() + "]同意.如有疑问请联系小助手,小助手微信号:yiyinglikeke";
 			} else if (nowState.equals(OrderService.ORDER_STATE_USER_REGRET)
 					&& lastState.equals(OrderService.ORDER_STATE_WAIT_SERVICE)) {
 				if (operation.equals(TRefuseOrderService.class.getName())) {
 					return "尊敬的用户，订单(订单号:" + No + ")已被导师[" + teacher.getName() + "]拒绝,拒绝理由:" + order.getRefuseReason()
-							+ ",您可预约其他优秀的导师哦,预付款将在24小时内退还到您的账户。";
+							+ ",您可预约其他优秀的导师哦,预付款将在24小时内退还到您的账户。如有疑问请联系小助手,小助手微信号:yiyinglikeke";
 				} else {
-					return "尊敬的用户，订单(订单号:" + No + ")已经申请取消,请等待[" + teacher.getName() + "]同意";
+					return "尊敬的用户，订单(订单号:" + No + ")已经申请取消,请等待[" + teacher.getName() + "]同意.如有疑问请联系小助手,小助手微信号:yiyinglikeke";
 				}
 			} else if (nowState.equals(OrderService.ORDER_STATE_WAIT_RETURN)
 					&& lastState.equals(OrderService.ORDER_STATE_FINISH_PAID)) {
-				return "尊敬的用户，订单(订单号:" + No + ")已经取消，我们将竭尽全力在24小时内为您全额退款";
+				return "尊敬的用户，订单(订单号:" + No + ")已经取消，我们将竭尽全力在24小时内为您全额退款.如有疑问请联系小助手,小助手微信号:yiyinglikeke";
 			} else if (nowState.equals(OrderService.ORDER_STATE_USER_DISLIKE)
 					&& lastState.equals(OrderService.ORDER_STATE_SERVICE_FINISH)) {
-				return "尊敬的用户,订单(订单号:" + No + ")已经申请退款,请等待[" + teacher.getName() + "]确认";
+				return "尊敬的用户,订单(订单号:" + No + ")已经申请退款,请等待[" + teacher.getName() + "]确认.如有疑问请联系小助手,小助手微信号:yiyinglikeke";
 			} else if (nowState.equals(OrderService.ORDER_STATE_SERVICE_FINISH)
 					&& lastState.equals(OrderService.ORDER_STATE_WAIT_SERVICE)) {
-				return "尊敬的用户,订单(订单号:" + No + "),[" + teacher.getName() + "]已经确认服务完毕.请到一英里平台确认本次服务完成.(系统会在1周后自动确认服务)";
+				return "尊敬的用户,订单(订单号:" + No + "),[" + teacher.getName() + "]已经确认服务完毕.请到一英里平台确认本次服务完成.(系统会在1周后自动确认服务).如有疑问请联系小助手,小助手微信号:yiyinglikeke";
 			} else if (nowState.equals(OrderService.ORDER_STATE_WAIT_RETURN)
 					&& lastState.equals(OrderService.ORDER_STATE_USER_DISLIKE)) {
-				return "尊敬的用户,订单(订单号:" + No + "),[" + teacher.getName() + "]已同意退款,我们将竭尽全力在24内退还到您的账户,请注意查收.";
+				return "尊敬的用户,订单(订单号:" + No + "),[" + teacher.getName() + "]已同意退款,我们将竭尽全力在24内退还到您的账户,请注意查收.如有疑问请联系小助手,小助手微信号:yiyinglikeke";
 			} else if (nowState.equals(OrderService.ORDER_STATE_WAIT_RETURN)
 					&& lastState.equals(OrderService.ORDER_STATE_USER_REGRET)) {
-				return "尊敬的用户,订单(订单号:" + No + "),[" + teacher.getName() + "]已同意退款,我们将竭尽全力在24内退还到您的账户,请注意查收.";
+				return "尊敬的用户,订单(订单号:" + No + "),[" + teacher.getName() + "]已同意退款,我们将竭尽全力在24内退还到您的账户,请注意查收.如有疑问请联系小助手,小助手微信号:yiyinglikeke";
 			} else if (nowState.equals(OrderService.ORDER_STATE_WAIT_COMMENT)
 					&& lastState.equals(OrderService.ORDER_STATE_SERVICE_FINISH)) {
-				return "尊敬的用户,订单(订单号:" + No + "),系统已经帮您自动确认本次服务,请到一英里平台对本次服务进行评价,留下您宝贵的意见.";
+				return "尊敬的用户,订单(订单号:" + No + "),系统已经帮您自动确认本次服务,请到一英里平台对本次服务进行评价,留下您宝贵的意见.如有疑问请联系小助手,小助手微信号:yiyinglikeke";
 			} else if (nowState.equals(OrderService.ORDER_STATE_WAIT_COMMENT)
 					&& lastState.equals(OrderService.ORDER_STATE_MANAGER_IN)) {
-				return "尊敬的用户，订单(订单号:" + No + "),客服已经协调结束，协调结果：拒绝此次退款。请到一英里平台对本次服务进行评价";
+				return "尊敬的用户，订单(订单号:" + No + "),客服已经协调结束，协调结果：拒绝此次退款。请到一英里平台对本次服务进行评价.如有疑问请联系小助手,小助手微信号:yiyinglikeke";
 			} else if (nowState.equals(OrderService.ORDER_STATE_END_FAILED)
 					&& lastState.equals(OrderService.ORDER_STATE_MANAGER_IN)) {
-				return "尊敬的用户，订单(订单号:" + No + "),客服已经协调结束，协调结果：拒绝此次退款。";
+				return "尊敬的用户，订单(订单号:" + No + "),客服已经协调结束，协调结果：拒绝此次退款。如有疑问请联系小助手,小助手微信号:yiyinglikeke";
 			} else if (nowState.equals(OrderService.ORDER_STATE_WAIT_RETURN)
 					&& lastState.equals(OrderService.ORDER_STATE_MANAGER_IN)) {
-				return "尊敬的用户，订单(订单号:" + No + "),客服已经协调结束，协调结果：同意此次退款。我们将在24小时内给您退款.";
+				return "尊敬的用户，订单(订单号:" + No + "),客服已经协调结束，协调结果：同意此次退款。我们将在24小时内给您退款.如有疑问请联系小助手,小助手微信号:yiyinglikeke";
 			} else if (nowState.equals(OrderService.ORDER_STATE_END_FAILED)
 					&& lastState.equals(OrderService.ORDER_STATE_RETURN_SUCCESS)) {
-				return "尊敬的用户，订单(订单号:" + No + "),已经成功退款,请注意查收.如有疑问请咨询客服.";
+				return "尊敬的用户，订单(订单号:" + No + "),已经成功退款,请注意查收.如有疑问请咨询客服.如有疑问请联系小助手,小助手微信号:yiyinglikeke";
 			} else if (nowState.equals(OrderService.ORDER_STATE_WAIT_RETURN)
 					&& lastState.equals(OrderService.ORDER_STATE_WAIT_SERVICE)) {
-				return "尊敬的用户，订单(订单号:" + No + "),[" + teacher.getName() + "]已经取消服务,本次预付款将在24小时内为您退款。";
+				return "尊敬的用户，订单(订单号:" + No + "),[" + teacher.getName() + "]已经取消服务,本次预付款将在24小时内为您退款。如有疑问请联系小助手,小助手微信号:yiyinglikeke";
 			} else if (nowState.equals(OrderService.ORDER_STATE_WAIT_ENSURETIME)
 					&& lastState.equals(OrderService.ORDER_STATE_FINISH_PAID)) {
-				return "尊敬的用户，订单(订单号:" + No + "),[" + teacher.getName() + "]已经接受,TA将通过您提供的联系方式与您联系,请等待TA与您确认服务时间。";
+				return "尊敬的用户，订单(订单号:" + No + "),[" + teacher.getName() + "]已经接受,TA将通过您提供的联系方式与您联系,请等待TA与您确认服务时间。如有疑问请联系小助手,小助手微信号:yiyinglikeke";
 			} else if (nowState.equals(OrderService.ORDER_STATE_WAIT_SERVICE)
 					&& lastState.equals(OrderService.ORDER_STATE_FINISH_PAID)) {
-				return "尊敬的用户，订单(订单号:" + No + "),[" + teacher.getName() + "]已经接受,TA将通过您提供的联系方式与您联系,请等待服务。";
+				return "尊敬的用户，订单(订单号:" + No + "),[" + teacher.getName() + "]已经接受,TA将通过您提供的联系方式与您联系,请等待服务。如有疑问请联系小助手,小助手微信号:yiyinglikeke";
 			} else if (nowState.equals(OrderService.ORDER_STATE_MANAGER_IN)
 					&& (OrderService.ORDER_STATE_USER_DISLIKE.equals(lastState)
 							|| OrderService.ORDER_STATE_USER_REGRET.equals(lastState))) {
-				return "尊敬的用户，订单(订单号:" + No + ")的退款申请已被[" + teacher.getName() + "]拒绝,客服将介入此订单,请耐心等待,我们会在48小时内与您联系。";
+				return "尊敬的用户，订单(订单号:" + No + ")的退款申请已被[" + teacher.getName() + "]拒绝,客服将介入此订单,请耐心等待,我们会在48小时内与您联系。如有疑问请联系小助手,小助手微信号:yiyinglikeke";
 			} else if (nowState.equals(OrderService.ORDER_STATE_WAIT_SERVICE)
 					&& lastState.equals(OrderService.ORDER_STATE_WAIT_ENSURETIME)) {
-				return "尊敬的用户，订单(订单号:" + No + "),[" + teacher.getName() + "]已确认服务时间为(" + order.getOkTime() + "),请等待服务.";
+				return "尊敬的用户，订单(订单号:" + No + "),[" + teacher.getName() + "]已确认服务时间为(" + order.getOkTime() + "),请等待服务.如有疑问请联系小助手,小助手微信号:yiyinglikeke";
 			} else if (nowState.equals(OrderService.ORDER_STATE_WAIT_RETURN)
 					&& lastState.equals(OrderService.ORDER_STATE_WAIT_ENSURETIME)) {
-				return "尊敬的用户，订单(订单号:" + No + ")已经被[" + teacher.getName() + "]取消,您可以在平台选择其他优秀的导师进行服务。本次预付款将在24小时内为您退款。";
+				return "尊敬的用户，订单(订单号:" + No + ")已经被[" + teacher.getName() + "]取消,您可以在平台选择其他优秀的导师进行服务。本次预付款将在24小时内为您退款。如有疑问请联系小助手,小助手微信号:yiyinglikeke";
 			} else {
 				return "";
 			}
